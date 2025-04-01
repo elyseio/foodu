0abe5e648eb527f8de90863eb57fe983f6c95c1a
elyseio <elyseio@proton.me>
2025-04-01 19:39:48 +0800

    added doc string on the function write_to_file

--- python/main.py
+++ python/main.py
@@ -4,6 +4,16 @@ import json
 
 
 def write_to_file(data: list, file_name: str):
+    '''
+    Write the data into a json file
+
+    Args:
+        data - list of dictionary
+        file_name - string of the file name
+
+    Returns:
+        None
+    '''
     print(f'Writing into {file_name}')
     with open(file_name, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4)

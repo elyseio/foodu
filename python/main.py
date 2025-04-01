3e27a5c82fe36e53dccb33cec31745ced78aff61
elyseio <elyseio@proton.me>
2025-04-01 19:37:59 +0800

    imported json and added a function to write data as json file

--- python/main.py
+++ python/main.py
@@ -1,5 +1,12 @@
 from curl_cffi import requests
 from bs4 import BeautifulSoup
+import json
+
+
+def write_to_file(data: list, file_name: str):
+    print(f'Writing into {file_name}')
+    with open(file_name, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
 
 
 def main():
